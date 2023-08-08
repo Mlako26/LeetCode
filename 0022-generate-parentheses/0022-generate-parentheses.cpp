d1fe1a98d001bf22b595ef000065d3a1b06a8034
Manuel Lakowsky <37757576+Mlako26@users.noreply.github.com>
2023-08-07 22:23:23 -0300

    Time: 0 ms (100.00%), Space: 16.2 MB (12.23%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,17 +1,22 @@
 class Solution {
 private:
     void solve(vector<string> &res, int n, int izq, int der, string s) {
+        if (s.size() == 2*n) {
+            res.push_back(s);
+            return;
+        }
+        
         if (izq < n) {
             string next = s;
             next.push_back('(');
             solve(res, n, izq+1, der, next);
         }
+        
         if (izq > der) {
             string next = s;
             next.push_back(')');
             solve(res, n, izq, der+1, next);
         }
-        if (izq == n && der == n) res.push_back(s);
     }
 public:
     vector<string> generateParenthesis(int n) {

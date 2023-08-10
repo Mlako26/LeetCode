4ec7d06ec568b029a1b606dd952c7241107ae64c
Manuel Lakowsky <37757576+Mlako26@users.noreply.github.com>
2023-08-09 23:11:12 -0300

    Time: 3 ms (74.13%), Space: 11 MB (50.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,38 +4,47 @@ public:
         // First I must find the pivot
         int i = 0, j = nums.size() - 1;
         
-        while (i < j - 1) {
-            int k = (i + j) / 2;
-            
-            if (nums[k] < nums[nums.size() - 1]) j = k;
-            else i = k;
-        }
-        
-        int pivot = nums[i] < nums[j] ? i : j;
-        
-        // Then I look for the element on one side or the other of the pivot
-        i = 0;
-        j = pivot - 1;
-        
         while (i <= j) {
             int k = (i + j) / 2;
             
             if (nums[k] == target) return k;
-            else if (nums[k] < target) i = k + 1;
-            else j = k - 1;
-        }
-        
-        i = pivot;
-        j = nums.size() - 1;
-        
-         while (i <= j) {
-            int k = (i + j) / 2;
             
-            if (nums[k] == target) return k;
-            else if (nums[k] < target) i = k + 1;
-            else j = k - 1;
+            if (nums[i] <= nums[k]) {
+                if (target < nums[k] && target >= nums[i]) j = k - 1;
+                else i = k + 1;
+            } else {
+                if (target > nums[k] && target <= nums[j]) i = k + 1;
+                else j = k - 1;
+            }
         }
         
-        return -1;     
+        return -1;
+        
+//         int pivot = nums[i] < nums[j] ? i : j;
+        
+//         // Then I look for the element on one side or the other of the pivot
+//         i = 0;
+//         j = pivot - 1;
+        
+//         while (i <= j) {
+//             int k = (i + j) / 2;
+            
+//             if (nums[k] == target) return k;
+//             else if (nums[k] < target) i = k + 1;
+//             else j = k - 1;
+//         }
+        
+//         i = pivot;
+//         j = nums.size() - 1;
+        
+//          while (i <= j) {
+//             int k = (i + j) / 2;
+            
+//             if (nums[k] == target) return k;
+//             else if (nums[k] < target) i = k + 1;
+//             else j = k - 1;
+//         }
+        
+//         return -1;     
     }
 };

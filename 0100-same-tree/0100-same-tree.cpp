b2e61d268bf2a2c401c88f7ebba03f638e0d03e2
Manuel Lakowsky <37757576+Mlako26@users.noreply.github.com>
2023-09-17 12:25:23 -0300

    Time: 5 ms (15.95%), Space: 10.3 MB (5.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,6 +15,6 @@ public:
         if (!p && !q) return true;
         if (!p || !q) return false;
         
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right) && p->val == q->val;
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
